39dc7c369213d4e53d8a09b3b944d8c7e0a8fe99
dekunukem <dekunukem@gmail.com>
2022-07-26 02:45:08 +0100

    button working

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -239,7 +239,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PWR_ON_Pin */
   GPIO_InitStruct.Pin = PWR_ON_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(PWR_ON_GPIO_Port, &GPIO_InitStruct);
