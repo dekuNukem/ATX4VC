c5652cfef7f7756b187cc8e4dbf370b8aef199fd
dekunukem <dekunukem@gmail.com>
2022-08-19 01:14:44 +0100

    updated plate

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -293,7 +293,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PWR_BTN_Pin */
   GPIO_InitStruct.Pin = PWR_BTN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(PWR_BTN_GPIO_Port, &GPIO_InitStruct);
 
 }
